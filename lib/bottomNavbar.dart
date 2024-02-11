0c404b1b17a1619e7a247d2965d9d00095a11922
Rahma Khaled <rahmabadr1>
2024-02-11 22:42:19 +0200

    اختارو اللى عايزينه

--- lib/bottomNavbar.dart
+++ lib/bottomNavbar.dart
@@ -1,5 +1,148 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_nav_bar/google_nav_bar.dart';
+// import 'package:knee_app/chat.dart';
+// import 'package:knee_app/exercise.dart';
+// import 'package:knee_app/help.dart';
+// import 'package:knee_app/radiology.dart';
+// import 'package:knee_app/rating_bar.dart';
+// import 'package:knee_app/x_rays_page.dart';
+//
+// class BottomNavBar extends StatefulWidget {
+//   static final GlobalKey<_BottomNavBarState> navKey =
+//   GlobalKey<_BottomNavBarState>();
+//   static final GlobalKey<NavigatorState> navigatorKey =
+//   GlobalKey<NavigatorState>();
+//
+//   const BottomNavBar({Key? key}) : super(key: key);
+//
+//   static void toggleBottomNavBar(bool show) {
+//     navKey.currentState?.toggleBottomNavBar(show);
+//   }
+//
+//   @override
+//   State<BottomNavBar> createState() => _BottomNavBarState();
+// }
+// class _BottomNavBarState extends State<BottomNavBar> {
+//   void toggleBottomNavBar(bool show) {
+//     setState(() {
+//       _showBottomNavBar = show;
+//     });
+//
+//     if (show && _selectedIndex == 1) {
+//       _navigatorKey.currentState?.maybePop();
+//     }
+//   }
+//
+//   List<Widget> screens = [
+//     XRaysPage(),
+//     Chat(),
+//     RadiologyPage(),
+//   ];
+//   int _selectedIndex = 0;
+//   bool _showBottomNavBar = true;
+//
+//   GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       key: BottomNavBar.navKey,
+//       backgroundColor: Color(0xFF06607B),
+//       bottomNavigationBar: _showBottomNavBar
+//           ? Container(
+//         decoration: BoxDecoration(
+//             borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+//             color: Color(0xFFF0F8FF),
+//             boxShadow: [
+//               BoxShadow(
+//                   blurRadius: 20,
+//                   color: Colors.black.withOpacity(.1))
+//             ]),
+//         child: SafeArea(
+//           child: Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
+//             child: GNav(
+//               gap: 3,
+//               activeColor: Color(0xFF06607B),
+//               iconSize: 26,
+//               padding:
+//               EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+//               duration: Duration(milliseconds: 600),
+//               tabs: const [
+//                 GButton(
+//                   icon: Icons.document_scanner_outlined,
+//                   iconColor: Color(0xFF06607B),
+//                   text: 'X-Ray',
+//                   iconSize: 26,
+//                   textSize: 26,
+//                 ),
+//                 GButton(
+//                   icon: Icons.chat_outlined,
+//                   iconColor: Color(0xFF06607B),
+//                   text: 'Chat',
+//                   iconSize: 26,
+//                   textSize: 26,
+//                 ),
+//                 GButton(
+//                   icon: Icons.history,
+//                   iconColor: Color(0xFF06607B),
+//                   text: 'Radiology',
+//                   iconSize: 26,
+//                   textSize: 26,
+//                 ),
+//               ],
+//               selectedIndex: _selectedIndex,
+//               onTabChange: (index) {
+//                 setState(() {
+//                   if (_selectedIndex != index) {
+//                     _selectedIndex = index;
+//                     _navigatorKey.currentState
+//                         ?.popUntil((route) => route.isFirst);
+//                   }
+//                   if (index == 1) {
+//                     Navigator.pushReplacement(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => Chat()),
+//                     );
+//                   }
+//                 });
+//               },
+//             ),
+//           ),
+//         ),
+//       )
+//           : SizedBox(), // Placeholder for when _showBottomNavBar is false
+//       body: Navigator(
+//         key: _navigatorKey,
+//         onGenerateRoute: (settings) {
+//           return MaterialPageRoute(
+//             settings: settings,
+//             builder: (context) {
+//               switch (settings.name) {
+//                 case '/help':
+//                   _selectedIndex = -1;
+//                   _showBottomNavBar = true; // Show the bottom nav bar
+//                   return Help();
+//                 case '/rating':
+//                   _selectedIndex = -1;
+//                   _showBottomNavBar = true; // Show the bottom nav bar
+//                   return RatingPage();
+//                 case '/exercise':
+//                   _selectedIndex = -1;
+//                   _showBottomNavBar = true; // Show the bottom nav bar
+//                   return ExercisePage();
+//                 default:
+//                 // If none of the above, show the selected screen
+//                   return screens[_selectedIndex];
+//               }
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:knee_app/chat.dart';
 import 'package:knee_app/exercise.dart';
 import 'package:knee_app/help.dart';
@@ -50,63 +193,49 @@ class _BottomNavBarState extends State<BottomNavBar> {
       key: BottomNavBar.navKey,
       backgroundColor: Color(0xFF06607B),
       bottomNavigationBar: _showBottomNavBar
-          ? Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.vertical(top: Radius.circular(5)),color:  Color(0xFFF0F8FF), boxShadow: [
-          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
-        ]),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
-            child: GNav(
-              gap: 3,
-              activeColor:  Color(0xFF06607B),
-              iconSize: 26,
-              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-              duration: Duration(milliseconds: 600),
-              tabs: [
-                GButton(
-                  icon: Icons.document_scanner_outlined,
-                  iconColor: Color(0xFF06607B) ,
-                  text: 'X-Ray',
-                  iconSize:26 ,
-                  textSize: 26,
-                ),
-                GButton(
-                  icon: Icons.chat_outlined,
-                  iconColor:Color(0xFF06607B) ,
-                  text: 'Chat',
-                  iconSize:26 ,
-                  textSize: 26,
-                ),
-                GButton(
-                  icon: Icons.history,
-                  iconColor:Color(0xFF06607B) ,
-                  text: 'Radiology',
-                  iconSize:26,
-                  textSize: 26,
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: (index) {
-                setState(() {
-                  if (_selectedIndex != index) {
-                    _selectedIndex = index;
-                    _navigatorKey.currentState
-                        ?.popUntil((route) => route.isFirst);
-                  }
-                  if (index == 1) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Chat()),
-                    );
-                  }
-                });
-              },
-            ),
+          ? BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            if (_selectedIndex != index) {
+              _selectedIndex = index;
+              _navigatorKey.currentState
+                  ?.popUntil((route) => route.isFirst);
+            }
+            if (index == 1) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => Chat()),
+              );
+            }
+          });
+        },
+        elevation: 0.0,
+        iconSize: 25,
+        selectedFontSize: 13,
+        unselectedFontSize: 13,
+        backgroundColor: Color(0xFFF0F8FF),
+        selectedItemColor:Color(0xFF06607B),
+        unselectedItemColor: Color(0xFF06607B),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.document_scanner_outlined),
+            // activeIcon: Icon(Icons.document_scanner),
+            label: 'X-Ray',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_outlined),
+            // activeIcon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            // activeIcon: Icon(Icons.history_sharp),
+            label: 'Radiology',
+          ),
+        ],
       )
-          : null,
+          : SizedBox(),
       body: Navigator(
         key: _navigatorKey,
         onGenerateRoute: (settings) {
@@ -116,15 +245,17 @@ class _BottomNavBarState extends State<BottomNavBar> {
               switch (settings.name) {
                 case '/help':
                   _selectedIndex = -1;
+                  _showBottomNavBar = true; // Show the bottom nav bar
                   return Help();
                 case '/rating':
                   _selectedIndex = -1;
+                  _showBottomNavBar = true; // Show the bottom nav bar
                   return RatingPage();
                 case '/exercise':
                   _selectedIndex = -1;
+                  _showBottomNavBar = true; // Show the bottom nav bar
                   return ExercisePage();
                 default:
-                // Default case
                   return screens[_selectedIndex];
               }
             },
@@ -134,4 +265,3 @@ class _BottomNavBarState extends State<BottomNavBar> {
     );
   }
 }
-
